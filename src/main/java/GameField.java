60a55626ebad9be9bec508ddc66e6dd8fc9ab847
ElenaR <llinusik@mail.ru>
2023-10-30 16:57:59 +0300

    Finish

--- src/main/java/GameField.java
+++ src/main/java/GameField.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
@@ -27,6 +29,8 @@ public class GameField extends JPanel implements ActionListener {
         setBackground(Color.black); //цвет игрового поля
         loadImages();
         initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);  //ФОКУС НА ИГРОВОЕ ПОЛЕ
             }
 
     public void initGame(){
@@ -45,9 +49,9 @@ public class GameField extends JPanel implements ActionListener {
     }
 
     public void loadImages(){
-        ImageIcon iia = new ImageIcon("apple.png"); //загружаем картинки
+        ImageIcon iia = new ImageIcon("src/main/apple.png"); //загружаем картинки
         apple = iia.getImage();
-        ImageIcon iid = new ImageIcon("dot.png");
+        ImageIcon iid = new ImageIcon("src/main/dot.png");
         dot = iid.getImage();
     }
 
@@ -59,8 +63,13 @@ public class GameField extends JPanel implements ActionListener {
             g.drawImage(apple, appleX, appleY, this);
             for (int i = 0; i <dots ; i++) {
                 g.drawImage(dot, x[i], y[i], this);
-
             }
+        } else {
+            String str = "Game over";
+            //Font f = new Font("Arial", 140, Font.BOLD);
+            g.setColor(Color.BLUE);
+           // g.setFont(f);
+            g.drawString(str, 125, SIZE/2);
         }
     }
 
@@ -82,12 +91,69 @@ public class GameField extends JPanel implements ActionListener {
             y[0] += DOT_SIZE; //
         }
     }
+
+    public void checkApple(){
+        if (x[0] == appleX && y[0]== appleY){
+            dots++;
+            createApple();
+        }
+    }
+
+    public void checkCollisions(){
+        for (int i = dots; i > 0 ; i--) {
+            if (i>4 && x[0] ==x[i] && y[0] == y[i]){ //столкновение самим с собой
+                inGame = false;
+            }
+        }
+        if (x[0] > SIZE){ //выход за поле
+            inGame = false;
+        }
+        if (x[0] < 0){
+            inGame = false;
+        }
+        if (y[0] > SIZE){
+            inGame = false;
+        }
+        if (y[0] < 0){
+            inGame = false;
+        }
+    }
     @Override
     public void actionPerformed(ActionEvent e) { //будет вызываться таймер каждые 250 млс
         if (inGame){
+            checkApple(); // если встретилось яблоко
+            checkCollisions();
             move();
         }
         repaint(); //перерисовываем поле
+    }
+
+    class FieldKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT && !right){
+                left = true;
+                up = false;
+                down = false;
+              }
+            if (key == KeyEvent.VK_RIGHT && !left){
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_UP && !down){
+                right = false;
+                up = true;
+                left = false;
+            }
+            if (key == KeyEvent.VK_DOWN && !up){
+                right = false;
+                down = true;
+                left = false;
+            }
+        }
     }
 }
 
